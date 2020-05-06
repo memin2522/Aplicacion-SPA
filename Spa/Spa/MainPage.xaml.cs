46436c210063dccfc6752d2af3a3c883988ade7f
Sergio <serrey040500@gmail.com>
2020-05-05 20:00:35 -0500

    Se añadio el proyecto a git

---
+++ Spa/Spa/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Spa
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void BtnTalleres_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavPrincipal());
+
+        }
+
+        async void BtnInfo_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavInfo());
+        }
+
+        async void BtnTips_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavTips());
+        }
+    }
+}
---
+++ Spa/Spa/NavegacionPrincipal.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Spa
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NavegacionPrincipal : TabbedPage
+    {
+        public NavegacionPrincipal()
+        {
+            InitializeComponent();
+        }
+    }
+}
